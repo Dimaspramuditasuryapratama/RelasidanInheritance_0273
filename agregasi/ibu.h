ed15c2edd1b205649f5b7cbfd849ded62ad41bcd
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-27 10:35:28 +0700

    Implement cetakAnak method in ibu class to display children's names

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -19,4 +19,15 @@ class ibu {
         void ibu::tambahkanAnak(anak* pAnak) {
             daftar_anak.push_back(pAnak);
         }
-};
+        void ibu::cetakAnak() {
+            cout << "Daftar anak dari Ibu \"" << this->nama << "\":\n";
+            // for (auto& a : daftar_anak) {
+            //    cout << a->nama << "\n";
+            //}
+            for (int i = 0; i < daftar_anak.size(); i++) {
+                cout << daftar_anak[i]->nama << endl;
+            }
+            cout << endl;
+        }
+        #endif
+    
