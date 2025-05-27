e27d60975a20a4cd9d965cd8bdc71f49a6e3fe0b
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-27 10:59:14 +0700

    Add dokter class implementation with constructor and destructor

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -18,3 +18,18 @@ class pasisen {     //Asosiasi
         void tambahkanDokter(dokter*);
         void cetakDokter();
 };
+
+class dokter {
+    public:
+        string nama;
+        vector<pasisen*> daftar_pasien;
+
+        dokter(string pNama): nama(pNama) {
+            cout << "Dokter \"" << nama << "\" ada\n";
+        }
+        ~dokter() {
+            cout << "Dokter \"" << nama << "\" tidak ada\n";
+        }
+        void tambahkanPasien(pasisen*);
+        void cetakPasien();
+};
