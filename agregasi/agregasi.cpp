1bcb2950d0248645e2000782711811c522809c84
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-05-27 10:47:32 +0700

    Menghapus varibu dan varanak

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -17,5 +17,12 @@ int main() {
     varIbu2->tambahkanAnak(varAnak3);
     varIbu2->tambahkanAnak(varAnak1);
 
-    
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
 }
